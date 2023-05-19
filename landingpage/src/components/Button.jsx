eecf59285f062ef6f76a6cf8a0aab2f8a5943a81
Dr-SIRM <91543656+Dr-SIRM@users.noreply.github.com>
2023-05-19 09:25:20 +0000

    React Landingpage

---
+++ landingpage/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <a
+    href="https://www.google.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  >
+    Get Started
+  </a>
+);
+
+export default Button;
